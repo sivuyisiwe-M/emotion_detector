7d9961dfe176b497b7cd27b45ce6e6eced14ce26
sivuyisiwe mgijima <sivuyisiwe.mgijima@capaciti.org.za>
2025-05-05 16:03:19 +0200

    error handling

--- emotion_detector/app/server.py
+++ emotion_detector/app/server.py
@@ -1,6 +1,32 @@
 
+# from flask import Flask, request, jsonify
+# from emotion_detector import emotion_predictor
+
+# app = Flask(__name__)
+
+# @app.route('/emotion', methods=['POST'])
+# def detect_emotion():
+#     data = request.get_json()
+#     if 'text' not in data:
+#         return jsonify({"error": "Missing text parameter"}), 400
+#     text = data['text']
+#     response = emotion_predictor(text)
+#     return jsonify(response)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
 from flask import Flask, request, jsonify
-from emotion_detector import emotion_predictor
+from app.emotion_detector import emotion_predictor
 
 app = Flask(__name__)
 
@@ -10,8 +36,22 @@ def detect_emotion():
     if 'text' not in data:
         return jsonify({"error": "Missing text parameter"}), 400
     text = data['text']
+    if not text:
+        return jsonify({"error": "Invalid text"}), 400
     response = emotion_predictor(text)
+    if "error" in response:
+        return jsonify(response), 400
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
